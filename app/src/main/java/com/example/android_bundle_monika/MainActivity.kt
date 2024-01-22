4a48c21b634da4cba874b02cb42ad44bf64b307c
uswtnnisa <uswatunnisa382@gmail.com>
2024-01-22 10:17:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android_bundle_monika/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.android_bundle_monika
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var namaEdit: EditText
+    private lateinit var umurEdit: EditText
+    private lateinit var submitBtn: Button
+
+    private val NAME_KEY: String = "nama"
+    private val AGE_KEY: String = "umur"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        namaEdit = findViewById(R.id.namaEdit)
+        umurEdit = findViewById(R.id.umurEdit)
+        submitBtn = findViewById(R.id.submitBtn)
+
+        submitBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra(NAME_KEY, namaEdit.text.toString())
+            intent.putExtra(AGE_KEY, umurEdit.text.toString())
+            startActivity(intent)
+        }
+    }
+}
+
+class MainActivity2 {
+
+}
